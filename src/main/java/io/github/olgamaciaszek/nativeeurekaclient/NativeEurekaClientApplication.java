d5cd2c96610997afda254074811ab37cbad44118
Olga MaciaszekSharma <olga.maciaszek@gmail.com>
2021-06-10 17:55:29 +0200

    Add kubernetes demo setup notes.

--- src/main/java/io/github/olgamaciaszek/nativeeurekaclient/NativeEurekaClientApplication.java
+++ src/main/java/io/github/olgamaciaszek/nativeeurekaclient/NativeEurekaClientApplication.java
@@ -2,12 +2,15 @@ package io.github.olgamaciaszek.nativeeurekaclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.metrics.buffering.BufferingApplicationStartup;
 
 @SpringBootApplication
 public class NativeEurekaClientApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(NativeEurekaClientApplication.class, args);
+		SpringApplication application = new SpringApplication(NativeEurekaClientApplication.class);
+		application.setApplicationStartup(new BufferingApplicationStartup(10000));
+		application.run(args);
 	}
 
 }
